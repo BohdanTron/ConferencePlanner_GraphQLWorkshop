d92c469fd3a7e977c1be756be75ec7be3281ce5b
tronbodya <tronbodya@gmail.com>
2023-10-08 14:18:03 +0300

    data loaders improvements

--- GraphQL/DataLoader/SessionByIdDataLoader.cs
+++ GraphQL/DataLoader/SessionByIdDataLoader.cs
@@ -5,19 +5,23 @@ namespace GraphQL.DataLoader
 {
     public class SessionByIdDataLoader : BatchDataLoader<int, Session>
     {
-        private readonly ApplicationDbContext _dbContext;
+        private readonly IServiceProvider _serviceProvider;
 
-        public SessionByIdDataLoader(IBatchScheduler batchScheduler, ApplicationDbContext dbContext)
+        public SessionByIdDataLoader(IBatchScheduler batchScheduler, IServiceProvider serviceProvider)
             : base(batchScheduler)
         {
-            _dbContext = dbContext;
+            _serviceProvider = serviceProvider;
         }
 
         protected override async Task<IReadOnlyDictionary<int, Session>> LoadBatchAsync(
             IReadOnlyList<int> keys,
             CancellationToken cancellationToken)
         {
-            return await _dbContext.Sessions
+            await using var scope = _serviceProvider.CreateAsyncScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            return await dbContext.Sessions
                 .Where(s => keys.Contains(s.Id))
                 .ToDictionaryAsync(t => t.Id, cancellationToken);
         }
--- GraphQL/DataLoader/SpeakerByIdDataLoader.cs
+++ GraphQL/DataLoader/SpeakerByIdDataLoader.cs
@@ -5,19 +5,23 @@ namespace GraphQL.DataLoader
 {
     public class SpeakerByIdDataLoader : BatchDataLoader<int, Speaker>
     {
-        private readonly ApplicationDbContext _dbContext;
+        private readonly IServiceProvider _serviceProvider;
 
-        public SpeakerByIdDataLoader(IBatchScheduler batchScheduler, ApplicationDbContext dbContext)
+        public SpeakerByIdDataLoader(IBatchScheduler batchScheduler, IServiceProvider serviceProvider)
             : base(batchScheduler)
         {
-            _dbContext = dbContext;
+            _serviceProvider = serviceProvider;
         }
 
         protected override async Task<IReadOnlyDictionary<int, Speaker>> LoadBatchAsync(
             IReadOnlyList<int> keys,
             CancellationToken cancellationToken)
         {
-            return await _dbContext.Speakers
+            await using var scope = _serviceProvider.CreateAsyncScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            return await dbContext.Speakers
                 .Where(s => keys.Contains(s.Id))
                 .ToDictionaryAsync(s => s.Id, cancellationToken);
         }
